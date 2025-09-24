5bc75352d82b9856799f4e4ac2821cf3f7a8da6c
Nadiia <luna77di@gmail.com>
2025-09-25 00:46:27 +0300

    vhgjgh

---
+++ homework8.py
@@ -0,0 +1,83 @@
+# #4
+while True:
+        a = input('Введіть слово: ').strip().lower()
+        b = input('Введіть слово: ').strip().lower()
+
+        ok = True
+        for ch in b:
+            if b.count(ch) > a.count(ch):
+                ok = False
+                break
+
+        print("Yes" if ok else "No")
+        break
+# #1
+text = input("Введіть текст: ")
+
+result = ""
+for ch in text:
+    if ch.isupper():
+        result += "*"
+    else:
+        result += ch
+
+print(result)
+#2
+lines = []  # ??????
+while True:
+    s = input()
+    if not s:
+        break
+    lines.append(s)
+
+for s in lines:
+    max_zeros = 0
+    count = 0
+    for ch in s:
+        if ch == '0':
+            count += 1
+            if count > max_zeros:
+                max_zeros = count
+        else:
+            count = 0
+    print(max_zeros)
+#3
+import string
+
+text = input("Введіть текст: ")
+
+# a. Символ, що трапляється найбільшу кількість разів
+#???
+
+# b. Кількість знаків пунктуації
+punct_count = sum(1 for ch in text if ch in string.punctuation)
+print("b. Кількість знаків пунктуації:", punct_count)
+
+# c. Літери алфавіту, що не були знайдені у тексті
+#???
+
+# d. Кількість унікальних символів (ті, що зустрічаються лише один раз)
+unique_count = sum(1 for ch in text if text.count(ch) == 1)
+print("d. Кількість унікальних символів:", unique_count)
+#5
+text = input("Введіть рядок: ")
+
+if not text:
+    print("")
+else:
+    result = ""
+    count = 1
+
+    for i in range(1, len(text)):
+        if text[i] == text[i-1]:
+            count += 1
+        else:
+            result += text[i-1] + str(count)
+            count = 1
+
+    result += text[-1] + str(count)
+
+    print("Закодований рядок:", result)
+
+
+
---
+++ tasks8.py
@@ -0,0 +1,68 @@
+# # #1
+# s = input("Введіть рядок: ")
+#
+# upper = 0
+# lower = 0
+# spaces = 0
+#
+# for ch in s:
+#     if ch.isupper():
+#         upper += 1
+#     elif ch.islower():
+#         lower += 1
+#     elif ch == " ":
+#         spaces += 1
+#
+# print("Upper", upper)
+# print("Lower", lower)
+# print("Spaces", spaces)
+# # #2
+# s = input("Введіть рядок: ")
+#
+# letters = 0
+# digits = 0
+#
+# for ch in s:
+#     if ch.isalpha():
+#         letters += 1
+#     elif ch.isdigit():
+#         digits += 1
+#
+# print("Letters", letters)
+# print("Digits", digits)
+# # #3
+# s = input("Введіть рядок: ")
+# n = int(input("Введіть n: "))
+#
+# result = s[:n].upper() + s[n:]
+# print(result)
+# # #4
+# s = input("Введіть рядок: ")
+#
+# result = " ".join(s.split())
+#
+# print(result)
+# # #5
+# s = input("Введіть рядок: ").strip()
+#
+# if len(s) > 1:
+#     new_s = s[-1] + s[1:-1] + s[0]
+# else:
+#     new_s = s
+#
+# print("Результат:", new_s)
+
+
+#6
+s = input("Введіть рядок: ") #TheOldSeaDogAtTheAdmiralBenbow
+
+result = ""
+for ch in s:
+    if ch.isupper() and result:
+        result += " " + ch
+    else:
+        result += ch
+
+print(result)
+
+
